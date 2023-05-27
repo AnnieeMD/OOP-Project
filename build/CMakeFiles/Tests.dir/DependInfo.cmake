50933ac4466ddf469359b99165d003a46fba5bdf
Ani Dermendzhieva <130653510+AnnieeMD@users.noreply.github.com>
2023-05-27 15:13:04 +0300

    Add files via upload

---
+++ build/CMakeFiles/Tests.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Project/Commands.cpp" "CMakeFiles/Tests.dir/Commands.cpp.obj" "gcc" "CMakeFiles/Tests.dir/Commands.cpp.obj.d"
+  "D:/Project/XMLElement.cpp" "CMakeFiles/Tests.dir/XMLElement.cpp.obj" "gcc" "CMakeFiles/Tests.dir/XMLElement.cpp.obj.d"
+  "D:/Project/XMLGeneratorT.cpp" "CMakeFiles/Tests.dir/XMLGeneratorT.cpp.obj" "gcc" "CMakeFiles/Tests.dir/XMLGeneratorT.cpp.obj.d"
+  "D:/Project/XMLParser.cpp" "CMakeFiles/Tests.dir/XMLParser.cpp.obj" "gcc" "CMakeFiles/Tests.dir/XMLParser.cpp.obj.d"
+  "D:/Project/XMLPath.cpp" "CMakeFiles/Tests.dir/XMLPath.cpp.obj" "gcc" "CMakeFiles/Tests.dir/XMLPath.cpp.obj.d"
+  "D:/Project/XMLTree.cpp" "CMakeFiles/Tests.dir/XMLTree.cpp.obj" "gcc" "CMakeFiles/Tests.dir/XMLTree.cpp.obj.d"
+  "D:/Project/testsForCommands.cpp" "CMakeFiles/Tests.dir/testsForCommands.cpp.obj" "gcc" "CMakeFiles/Tests.dir/testsForCommands.cpp.obj.d"
+  "D:/Project/testsForElement.cpp" "CMakeFiles/Tests.dir/testsForElement.cpp.obj" "gcc" "CMakeFiles/Tests.dir/testsForElement.cpp.obj.d"
+  "D:/Project/testsForGenerator.cpp" "CMakeFiles/Tests.dir/testsForGenerator.cpp.obj" "gcc" "CMakeFiles/Tests.dir/testsForGenerator.cpp.obj.d"
+  "D:/Project/testsForParser.cpp" "CMakeFiles/Tests.dir/testsForParser.cpp.obj" "gcc" "CMakeFiles/Tests.dir/testsForParser.cpp.obj.d"
+  "D:/Project/testsForPath.cpp" "CMakeFiles/Tests.dir/testsForPath.cpp.obj" "gcc" "CMakeFiles/Tests.dir/testsForPath.cpp.obj.d"
+  "D:/Project/testsForTree.cpp" "CMakeFiles/Tests.dir/testsForTree.cpp.obj" "gcc" "CMakeFiles/Tests.dir/testsForTree.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Tests.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tests.dir/Commands.cpp.obj"
+  "CMakeFiles/Tests.dir/Commands.cpp.obj.d"
+  "CMakeFiles/Tests.dir/XMLElement.cpp.obj"
+  "CMakeFiles/Tests.dir/XMLElement.cpp.obj.d"
+  "CMakeFiles/Tests.dir/XMLGeneratorT.cpp.obj"
+  "CMakeFiles/Tests.dir/XMLGeneratorT.cpp.obj.d"
+  "CMakeFiles/Tests.dir/XMLParser.cpp.obj"
+  "CMakeFiles/Tests.dir/XMLParser.cpp.obj.d"
+  "CMakeFiles/Tests.dir/XMLPath.cpp.obj"
+  "CMakeFiles/Tests.dir/XMLPath.cpp.obj.d"
+  "CMakeFiles/Tests.dir/XMLTree.cpp.obj"
+  "CMakeFiles/Tests.dir/XMLTree.cpp.obj.d"
+  "CMakeFiles/Tests.dir/testsForCommands.cpp.obj"
+  "CMakeFiles/Tests.dir/testsForCommands.cpp.obj.d"
+  "CMakeFiles/Tests.dir/testsForElement.cpp.obj"
+  "CMakeFiles/Tests.dir/testsForElement.cpp.obj.d"
+  "CMakeFiles/Tests.dir/testsForGenerator.cpp.obj"
+  "CMakeFiles/Tests.dir/testsForGenerator.cpp.obj.d"
+  "CMakeFiles/Tests.dir/testsForParser.cpp.obj"
+  "CMakeFiles/Tests.dir/testsForParser.cpp.obj.d"
+  "CMakeFiles/Tests.dir/testsForPath.cpp.obj"
+  "CMakeFiles/Tests.dir/testsForPath.cpp.obj.d"
+  "CMakeFiles/Tests.dir/testsForTree.cpp.obj"
+  "CMakeFiles/Tests.dir/testsForTree.cpp.obj.d"
+  "Tests.exe"
+  "Tests.exe.manifest"
+  "Tests.pdb"
+  "libTests.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
