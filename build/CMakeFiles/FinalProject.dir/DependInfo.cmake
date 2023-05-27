05d6dfcda3b6314031ffe2fd9068582e127141e4
Ani Dermendzhieva <130653510+AnnieeMD@users.noreply.github.com>
2023-05-27 15:09:51 +0300

    Add files via upload

---
+++ build/CMakeFiles/FinalProject.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Project/Commands.cpp" "CMakeFiles/FinalProject.dir/Commands.cpp.obj" "gcc" "CMakeFiles/FinalProject.dir/Commands.cpp.obj.d"
+  "D:/Project/Runner.cpp" "CMakeFiles/FinalProject.dir/Runner.cpp.obj" "gcc" "CMakeFiles/FinalProject.dir/Runner.cpp.obj.d"
+  "D:/Project/XMLElement.cpp" "CMakeFiles/FinalProject.dir/XMLElement.cpp.obj" "gcc" "CMakeFiles/FinalProject.dir/XMLElement.cpp.obj.d"
+  "D:/Project/XMLGeneratorT.cpp" "CMakeFiles/FinalProject.dir/XMLGeneratorT.cpp.obj" "gcc" "CMakeFiles/FinalProject.dir/XMLGeneratorT.cpp.obj.d"
+  "D:/Project/XMLParser.cpp" "CMakeFiles/FinalProject.dir/XMLParser.cpp.obj" "gcc" "CMakeFiles/FinalProject.dir/XMLParser.cpp.obj.d"
+  "D:/Project/XMLPath.cpp" "CMakeFiles/FinalProject.dir/XMLPath.cpp.obj" "gcc" "CMakeFiles/FinalProject.dir/XMLPath.cpp.obj.d"
+  "D:/Project/XMLTree.cpp" "CMakeFiles/FinalProject.dir/XMLTree.cpp.obj" "gcc" "CMakeFiles/FinalProject.dir/XMLTree.cpp.obj.d"
+  "D:/Project/main.cpp" "CMakeFiles/FinalProject.dir/main.cpp.obj" "gcc" "CMakeFiles/FinalProject.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/FinalProject.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FinalProject.dir/Commands.cpp.obj"
+  "CMakeFiles/FinalProject.dir/Commands.cpp.obj.d"
+  "CMakeFiles/FinalProject.dir/Runner.cpp.obj"
+  "CMakeFiles/FinalProject.dir/Runner.cpp.obj.d"
+  "CMakeFiles/FinalProject.dir/XMLElement.cpp.obj"
+  "CMakeFiles/FinalProject.dir/XMLElement.cpp.obj.d"
+  "CMakeFiles/FinalProject.dir/XMLGeneratorT.cpp.obj"
+  "CMakeFiles/FinalProject.dir/XMLGeneratorT.cpp.obj.d"
+  "CMakeFiles/FinalProject.dir/XMLParser.cpp.obj"
+  "CMakeFiles/FinalProject.dir/XMLParser.cpp.obj.d"
+  "CMakeFiles/FinalProject.dir/XMLPath.cpp.obj"
+  "CMakeFiles/FinalProject.dir/XMLPath.cpp.obj.d"
+  "CMakeFiles/FinalProject.dir/XMLTree.cpp.obj"
+  "CMakeFiles/FinalProject.dir/XMLTree.cpp.obj.d"
+  "CMakeFiles/FinalProject.dir/main.cpp.obj"
+  "CMakeFiles/FinalProject.dir/main.cpp.obj.d"
+  "FinalProject.exe"
+  "FinalProject.exe.manifest"
+  "FinalProject.pdb"
+  "libFinalProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FinalProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
